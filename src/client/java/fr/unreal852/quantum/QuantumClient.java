d31a837d91e5470ac62ff218172019e073fb82a4
Dorian <dodo-pl-08@hotmail.fr>
2024-07-16 00:26:11 +0200

    Initial commit

---
+++ src/client/java/fr/unreal852/quantum/QuantumClient.java
@@ -0,0 +1,10 @@
+package fr.unreal852.quantum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuantumClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
