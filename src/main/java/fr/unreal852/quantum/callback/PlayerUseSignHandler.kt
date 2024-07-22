2f4a9811d1b5192028ccedc51f9d21da7690d2c5
Dorian <dodo-pl-08@hotmail.fr>
2024-07-23 00:19:38 +0200

    Signs can now be used to teleport to any world.

--- src/main/java/fr/unreal852/quantum/callback/PlayerUseSignHandler.kt
+++ src/main/java/fr/unreal852/quantum/callback/PlayerUseSignHandler.kt
@@ -1,29 +1,46 @@
 ﻿package fr.unreal852.quantum.callback
 
-import fr.unreal852.quantum.Quantum.Companion.LOGGER
+import fr.unreal852.quantum.utils.Extensions.teleportTo
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.minecraft.block.SignBlock
+import net.minecraft.block.WallSignBlock
+import net.minecraft.block.entity.SignBlockEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.Identifier
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.world.World
 
 class PlayerUseSignHandler : UseBlockCallback {
-    override fun interact(player: PlayerEntity?, world: World?, hand: Hand?, hitResult: BlockHitResult?): ActionResult {
+    override fun interact(player: PlayerEntity, world: World, hand: Hand, hitResult: BlockHitResult): ActionResult {
 
-        if (world?.isClient == null || player == null || hitResult == null)
-            return ActionResult.PASS
         if (world.isClient || player.isSpectator || player.isSneaking)
             return ActionResult.PASS
 
         val blockState = world.getBlockState(hitResult.blockPos)
         val block = blockState.block
 
-        if (block is SignBlock) {
-            LOGGER.info("Sign block found.")
-            return ActionResult.SUCCESS
+        if (block !is SignBlock && block !is WallSignBlock)
+            return ActionResult.PASS
+
+        val signEntity = world.getBlockEntity(hitResult.blockPos)
+
+        if (signEntity is SignBlockEntity) {
+
+            val text = signEntity.backText.getMessage(0, false).string
+            val identifier = Identifier.of(text)
+            val targetWorld = world.server?.getWorld(RegistryKey.of(RegistryKeys.WORLD, identifier))
+
+            if (targetWorld is ServerWorld) {
+                player.teleportTo(targetWorld, targetWorld.spawnPos.toBottomCenterPos())
+                return ActionResult.SUCCESS
+            }
         }
+
         return ActionResult.PASS
     }
 }
--- src/main/java/fr/unreal852/quantum/utils/Extensions.kt
+++ src/main/java/fr/unreal852/quantum/utils/Extensions.kt
@@ -1,10 +1,20 @@
 ﻿package fr.unreal852.quantum.utils
 
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.TeleportTarget
 
 object Extensions {
+
     fun NbtCompound.getIdentifier(key: String): Identifier {
         return Identifier.of(this.getString(key))
+    }
+
+    fun PlayerEntity.teleportTo(targetWorld: ServerWorld, pos: Vec3d) {
+        val teleportTarget = TeleportTarget(targetWorld, pos, Vec3d.ZERO, 0f, 0f, false) {}
+        this.teleportTo(teleportTarget)
     }
 }
