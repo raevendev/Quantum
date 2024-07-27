44c07c22a9e337613ea4e8a1180738d9b77d9d45
Unreal <dodo-pl-08@hotmail.fr>
2024-07-27 15:11:03 +0200

    Implemented the command to set a sign destination

--- src/main/java/fr/unreal852/quantum/command/SetTeleportSignCommand.kt
+++ src/main/java/fr/unreal852/quantum/command/SetTeleportSignCommand.kt
@@ -2,15 +2,22 @@ package fr.unreal852.quantum.command
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.context.CommandContext
 import fr.unreal852.quantum.Quantum
+import fr.unreal852.quantum.command.suggestion.WorldsDimensionSuggestionProvider
 import fr.unreal852.quantum.utils.TextUtils
 import net.minecraft.block.SignBlock
 import net.minecraft.block.WallSignBlock
+import net.minecraft.block.entity.SignBlockEntity
+import net.minecraft.block.entity.SignText
+import net.minecraft.command.argument.DimensionArgumentType
+import net.minecraft.command.argument.IdentifierArgumentType
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import net.minecraft.world.RaycastContext
 
@@ -31,7 +38,7 @@ class SetTeleportSignCommand : Command<ServerCommandSource> {
 
                     val raycastContext = RaycastContext(
                         player.eyePos,
-                        player.eyePos.add(player.rotationVecClient.multiply(200.0)),
+                        player.eyePos.add(player.rotationVecClient.multiply(20.0)),
                         RaycastContext.ShapeType.OUTLINE,
                         RaycastContext.FluidHandling.NONE,
                         player
@@ -39,12 +46,28 @@ class SetTeleportSignCommand : Command<ServerCommandSource> {
 
                     val hitResult = world.raycast(raycastContext)
                     val blockState = world.getBlockState(hitResult.blockPos)
-                    Quantum.LOGGER.info(blockState.block.name.toString())
+
                     if (blockState.block !is SignBlock && blockState.block !is WallSignBlock) {
                         context.source.sendMessage(TextUtils.literal("You must look at a sign block", Formatting.RED))
                         return 0
                     }
 
+                    val worldName = IdentifierArgumentType.getIdentifier(context, "world")
+                    val world = context.source.server.getWorld(RegistryKey.of(RegistryKeys.WORLD, worldName))
+
+                    if (world == null) {
+                        context.source.sendMessage(TextUtils.literal("No world found with id '$worldName'", Formatting.RED))
+                        return 0
+                    }
+
+                    val signEntity = world.getBlockEntity(hitResult.blockPos) as SignBlockEntity? ?: return 0
+
+                    signEntity.changeText({
+                        SignText()
+                            .withMessage(0, Text.literal("teleport"))
+                            .withMessage(1, Text.literal(worldName.namespace))
+                            .withMessage(2, Text.literal(worldName.path))
+                    }, false)
                 }
 
             } catch (e: Exception) {
@@ -63,7 +86,8 @@ class SetTeleportSignCommand : Command<ServerCommandSource> {
                     .then(
                         CommandManager.literal("setdestination")
                             .then(
-                                CommandManager.argument("spawnRadius", IntegerArgumentType.integer(0))
+                                CommandManager.argument("world", DimensionArgumentType.dimension())
+                                    .suggests(WorldsDimensionSuggestionProvider())
                                     .executes(SetTeleportSignCommand())
                             )
                             .executes(SetTeleportSignCommand())
